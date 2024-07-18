74d5ae5c9b706fe91897e01aa4ce40b12e3468f7
giselecoder <gisele.tavares@yuzer.com.br>
2024-07-18 12:21:00 -0300

    Complemento do Item 41

--- src/Cap6/marker_interface/ShippingCalculator.java
+++ src/Cap6/marker_interface/ShippingCalculator.java
@@ -1,9 +1,11 @@
 package Cap6.marker_interface;
 
 public class ShippingCalculator {
+
     public static double calculateShipping(PhysicalProduct product) {
         // Implement logic to calculate shipping cost
         // For example, fixed rate for demonstration
         return 10.0;
     }
+
 }
