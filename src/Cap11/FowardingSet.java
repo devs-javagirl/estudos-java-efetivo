3ab9dadfe23b29e3e952eede8f8203a9440ebada
minamicr <minamicr@yahoo.com.br>
2024-12-19 19:19:45 -0300

    Item 79 - evite a sincronização excessiva

---
+++ src/Cap11/FowardingSet.java
@@ -0,0 +1,66 @@
+package Cap11;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class FowardingSet<E> implements Set<E> {
+  private final Set<E> s;
+
+  public FowardingSet(Set<E> s) {
+    this.s = s;
+  }
+
+  public int size() {
+    return s.size();
+  }
+
+  public boolean isEmpty() {
+    return s.isEmpty();
+  }
+
+  public boolean contains(Object o) {
+    return s.contains(o);
+  }
+
+  public Iterator<E> iterator() {
+    return s.iterator();
+  }
+
+  public Object[] toArray() {
+    return s.toArray();
+  }
+
+  public <T> T[] toArray(T[] a) {
+    return s.toArray(a);
+  }
+
+  public boolean add(E e) {
+    return s.add(e);
+  }
+
+  public boolean remove(Object o) {
+    return s.remove(o);
+  }
+
+  public boolean containsAll(Collection<?> c) {
+    return s.containsAll(c);
+  }
+
+  public boolean addAll(Collection<? extends E> c) {
+    return s.addAll(c);
+  }
+
+  public boolean retainAll(Collection<?> c) {
+    return s.retainAll(c);
+  }
+
+  public boolean removeAll(Collection<?> c) {
+    return s.removeAll(c);
+  }
+
+  public void clear() {
+    s.clear();
+  }
+}
---
+++ src/Cap11/ObserverExecution.java
@@ -0,0 +1,34 @@
+package Cap11;
+
+import java.util.HashSet;
+
+public class ObserverExecution {
+    public static void main(String[] args) {
+        ObservableSet<Integer> set = new ObservableSet<>(new HashSet<>());
+        populateSet(set);
+        //populateWithRemove(set);
+    }
+
+    private static void populateSet(ObservableSet<Integer> set) {
+
+        set.addObserver((s, e) -> System.out.println("tamanho " + s.size() + " elemento " + e));
+
+        for (int i = 0; i < 100; i++)
+            set.add(i);
+    }
+
+    private static void populateWithRemove(ObservableSet<Integer> set) {
+        set.addObserver(new SetObserver<>() {
+            public void added(ObservableSet<Integer> s, Integer e) {
+                System.out.println(e);
+                if (e == 23) {
+                    System.out.println("removing item 23");
+                    s.removeObserver(this);
+                }
+            }
+        });
+
+        for (int i = 0; i < 100; i++)
+            set.add(i);
+    }
+}
